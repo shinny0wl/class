3f30a64c463da9e15865211982a1ad593c0fbafe
Pau <paulina.floress@iteso.mx>
2023-10-03 14:18:53 -0600

    cambios

--- codigo1.py
+++ codigo1.py
@@ -1,4 +1,5 @@
 print("holaa, esta bien chido mi jueguito")
+print("LOL es para nacos")
 
 // Colocando un #%% es como una separacion entre codigo y codigo
 // Siempre que se hace una modificacion, se tiene que colocar un git add y luego git commit
