a184dd44b1abb2a60fadc5ddfb490faa45a0d29d
Pau <paulina.floress@iteso.mx>
2023-10-03 13:32:41 -0600

    puse que me gustan las papitas

---
+++ codigo1.py
@@ -0,0 +1,4 @@
+print("holaa, me gustan las papitas :D")
+
+// Colocando un #%% es como una separacion entre codigo y codigo
+// Siempre que se hace una modificacion, se tiene que colocar un git add y luego git commit
